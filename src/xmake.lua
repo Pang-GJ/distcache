384ce1c3aa81f7711883f7a933412dffdac0746a
Pang-GJ <2362421192@qq.com>
2023-01-15 15:32:08 +0800

    init project

--- src/xmake.lua
+++ src/xmake.lua
@@ -2,7 +2,7 @@ add_requires("spdlog", {system=false, configs = {header_only = true, fmt_externa
 add_requireconfs("spdlog.fmt", {override = true, version = "9.1.0", configs = {header_only = true}})
 add_requires("nlohmann_json", {system=false})
 
-target("mylib")
+target("distcache")
   set_kind("shared")
   add_files("$(projectdir)/src/**.cpp")
   add_includedirs("$(projectdir)/src", { public = true})
--- tests/xmake.lua
+++ tests/xmake.lua
@@ -1,7 +1,11 @@
 add_requires("doctest")
+add_requires("fmt")
 
-target("test_hello")
+target("test_lru")
   set_kind("binary")
   set_group("tests")
-  add_files("$(projectdir)/tests/test_hello.cpp")
-  add_packages("doctest")
+  add_files("$(projectdir)/tests/test_lru.cpp")
+  add_packages("doctest", "fmt")
+  add_deps("distcache")
+
+
